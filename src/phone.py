d2e2000c8a57a4bb37ddef65113e61db01ccb88f
MariyaParus <cat-she@mail.ru>
2023-06-15 18:27:27 +0300

    homework-4 fixed phone.py

--- src/phone.py
+++ src/phone.py
@@ -5,7 +5,7 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
     def __str__(self):
         return self.name
